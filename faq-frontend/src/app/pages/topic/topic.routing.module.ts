c613b9fd0382fadfac0ba4822e4c59acefa59a69
Gustavo Chinaglia <gustavo.chinaglia@ssi-schaefer.com>
2022-10-18 14:22:40 -0300

    Removing angular

--- faq-frontend/src/app/pages/topic/topic.routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { TopicComponent } from './topic.component';
-
-@NgModule({
-  imports: [RouterModule.forChild([{ path: '', component: TopicComponent }])],
-  exports: [RouterModule],
-})
-export class TopicRoutingModule {}

2d2c598e3217ea54539b23075244b3f3c23d3dd3
Gustavo Chinaglia <gustavo.chinaglia@ssi-schaefer.com>
2022-10-18 11:08:57 -0300

    First version for Topic

---
+++ faq-server/src/main/kotlin/com/myfaq/server/service/TopicServiceImpl.kt
@@ -0,0 +1,28 @@
+package com.myfaq.server.service
+
+import com.myfaq.server.core.exception.NotFoundException
+import com.myfaq.server.model.Topic
+import com.myfaq.server.repository.ITopicRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TopicServiceImpl(val topicRepository: ITopicRepository) : IBasicService<Topic> {
+
+    override fun save(entity: Topic): Topic {
+        return topicRepository.save(entity)
+    }
+
+    override fun saveAll(entities: MutableList<Topic>): MutableList<Topic> {
+        return topicRepository.saveAll(entities)
+    }
+
+    override fun findAll(): MutableList<Topic> {
+        return topicRepository.findAll()
+    }
+
+    override fun findById(id: Long): Topic {
+        return topicRepository.findById(id).orElseThrow {
+            NotFoundException("Topic", id)
+        }
+    }
+}

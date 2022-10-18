9081a2185aa228723ce6556938a77fb87d89a798
Gustavo Chinaglia <gustavo.chinaglia@ssi-schaefer.com>
2022-10-18 13:42:01 -0300

    Add created Data

--- faq-server/src/main/kotlin/com/myfaq/server/model/Topic.kt
+++ faq-server/src/main/kotlin/com/myfaq/server/model/Topic.kt
@@ -2,6 +2,8 @@ package com.myfaq.server.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.util.Calendar
 
 @Document
 data class Topic(
@@ -12,4 +14,6 @@ data class Topic(
     var id: String? = null
 
     var tags: List<String>? = null
+
+    val createdDate: LocalDateTime = LocalDateTime.now()
 }

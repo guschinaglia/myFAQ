dc4e33184046b07270bc458a777c0f3bd393f36e
Gustavo Chinaglia <gustavo.chinaglia@ssi-schaefer.com>
2022-10-18 10:13:00 -0300

    First commit

---
+++ faq-server/src/main/kotlin/com/gxtech/faq/FaqApplication.kt
@@ -0,0 +1,11 @@
+package com.gxtech.faq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FaqApplication
+
+fun main(args: Array<String>) {
+	runApplication<FaqApplication>(*args)
+}

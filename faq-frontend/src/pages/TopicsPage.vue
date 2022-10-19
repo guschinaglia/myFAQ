9ae7a7db66d84ddb2f6ac44e4d9054dc9bb1d4d7
Gustavo Chinaglia <gustavo.chinaglia@ssi-schaefer.com>
2022-10-19 18:24:13 -0300

    Modifying the base layout and adding topic

---
+++ faq-frontend/src/pages/TopicsPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <q-input rounded outlined dense>
+      <template v-slot:prepend>
+        <q-icon name="search" />
+      </template>
+      <template v-slot:append>
+        <q-btn icon="send" round flat />
+      </template>
+    </q-input>
+  </div>
+  <div class="q-pt-md"></div>
+  <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-fab color="primary"></q-fab>
+  </q-page-sticky>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  title: 'TopicsPage',
+});
+</script>
